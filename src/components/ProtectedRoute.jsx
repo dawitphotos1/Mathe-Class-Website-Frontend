2676dc71ac276940e85327872fb214c7ab893e57
Dawit <dawitphotos1@gmail.com>
2025-08-18 13:37:47 -0400

    Add table creation and test course in server.js

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,27 +1,3 @@
-// import React, { useContext } from "react";
-// import { Navigate } from "react-router-dom";
-// import { AuthContext } from "../context/AuthContext";
-
-// const ProtectedRoute = ({ allowedRoles = [], children }) => {
-//   const { user } = useContext(AuthContext);
-
-//   if (!user) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
-//     return <Navigate to="/unauthorized" />;
-//   }
-
-//   return children;
-// };
-
-// export default ProtectedRoute;
-
-
-
-
-
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
