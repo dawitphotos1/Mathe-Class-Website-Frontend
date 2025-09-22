21fa16cbaca8fd1bb2abb5239f6c4781e57dce8a
dawitphotos1 <dawitphotos1@gmail.com>
2025-09-22 13:11:47 -0400

    Add table creation and test course in server.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,23 +1,3 @@
-// // src/utils/axiosInstance.js
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: "https://mathe-class-website-backend-1.onrender.com/api/v1",
-//   withCredentials: true, // ✅ allow cookies
-// });
-
-// // Interceptor for debug
-// axiosInstance.interceptors.request.use((config) => {
-//   console.log("🔗 Sending request:", config.method?.toUpperCase(), config.url);
-//   return config;
-// });
-
-// export default axiosInstance;
-
-
-
-
-
 
 // src/utils/axiosInstance.js
 import axios from "axios";
