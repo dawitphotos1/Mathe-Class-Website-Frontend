03885e5b9bae62e2e438a4587098e3c80c7d32c5
dawitphotos1 <dawitphotos1@gmail.com>
2025-09-24 06:55:42 -0400

    Add table creation and test course in server.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,27 +1,4 @@
 
-// import axios from "axios";
-
-// const API_URL = "http://localhost:5000/api/v1";
-
-// // 🔐 Login
-// export const login = (payload) =>
-//   axios.post(`${API_URL}/auth/login`, payload, { withCredentials: true });
-
-// // 📝 Register
-// export const register = (payload) =>
-//   axios.post(`${API_URL}/auth/register`, payload, { withCredentials: true });
-
-// // 👤 Get current user
-// export const getCurrentUser = () =>
-//   axios.get(`${API_URL}/auth/me`, { withCredentials: true });
-
-// // 🚪 Logout
-// export const logout = () =>
-//   axios.post(`${API_URL}/auth/logout`, {}, { withCredentials: true });
-
-
-
-
 // services/authService.js
 import axios from "axios";
 
