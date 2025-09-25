65d0e8ce26feda05edc48b46e5e76194f1cc94ce
dawitphotos1 <dawitphotos1@gmail.com>
2025-09-24 20:15:57 -0400

    Add table creation and test course in server.js

--- src/Pages/AdminManageUsers.jsx
+++ src/Pages/AdminManageUsers.jsx
@@ -1,18 +1,3 @@
-// // src/Pages/AdminManageUsers.jsx
-// import React from "react";
-
-// const AdminManageUsers = () => {
-//   return (
-//     <div className="p-6">
-//       <h2 className="text-2xl font-bold mb-4">Manage Users</h2>
-//       <p>This is the placeholder page for managing users.</p>
-//     </div>
-//   );
-// };
-
-// export default AdminManageUsers;
-
-
 
 
 // src/Pages/AdminManageUsers.jsx
