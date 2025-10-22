09be491b5f3701a97ab6dc73cf9dbf55795bfecc
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-21 21:18:07 -0400

    Add table creation and test course in server.js

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,39 +1,4 @@
 
-// // src/components/AdminLayout.jsx
-// import React from "react";
-// import { Outlet } from "react-router-dom";
-// import { useAuth } from "../context/AuthContext";
-// import AdminSidebar from "./AdminSidebar";
-// import Loading from "./Loading";
-// import "../Pages/AdminDashboard.css";
-
-// const AdminLayout = () => {
-//   const { user, loading, checked } = useAuth();
-
-//   if (loading || !checked) {
-//     return <Loading />;
-//   }
-
-//   return (
-//     <div className="admin-layout">
-//       {/* Sidebar */}
-//       <AdminSidebar />
-
-//       {/* Main Content */}
-//       <main className="admin-main-content">
-//         <Outlet />
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default AdminLayout;
-
-
-
-
-
-
 // src/components/AdminLayout.jsx - UPDATED
 import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
--- src/components/CSSDebug.jsx
+++ src/components/CSSDebug.jsx
@@ -1,22 +1,3 @@
-// // src/components/CSSDebug.jsx
-// import React from "react";
-// import "./AdminDashboard.css";
-
-// const CSSDebug = () => {
-//   return (
-//     <div style={{ display: "none" }}>
-//       {/* This forces CSS to load */}
-//       <div className="admin-layout debug"></div>
-//       <div className="admin-sidebar debug"></div>
-//       <div className="admin-main-content debug"></div>
-//     </div>
-//   );
-// };
-
-// export default CSSDebug;
-
-
-
 // src/components/CSSDebug.jsx
 import React from 'react';
 import '../Pages/AdminDashboard.css';
