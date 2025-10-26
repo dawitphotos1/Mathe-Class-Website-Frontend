d5cca7960d0da6ccae3502c0684ed4853c38249a
dawitphotos1 <dawitphotos1@gmail.com>
2025-10-26 12:51:13 -0400

    Add table creation and test course in server.js

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,30 @@
+// import React from "react";
+// import Navbar from "./Navbar";
+
+
+// const Layout = ({ user, onLogout, children }) => (
+//   <>
+//     <Navbar user={user} onLogout={onLogout} />
+//     <main>{children}</main>
+//     {/* <ToastContainer /> */}
+//   </>
+// );
+
+// export default Layout;
+
+
+
+
+
+
 import React from "react";
 import Navbar from "./Navbar";
-import { ToastContainer } from "react-toastify";
 
 const Layout = ({ user, onLogout, children }) => (
   <>
     <Navbar user={user} onLogout={onLogout} />
     <main>{children}</main>
-    <ToastContainer />
+    {/* <ToastContainer /> */}
   </>
 );
 
