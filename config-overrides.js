bc02090345c045748ffdafe5e2fa6c48f88605ef
dawitphotos1 <dawitphotos1@gmail.com>
2025-12-08 10:00:08 -0500

    Add table creation and test course in server.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,4 @@
 
-// const { override, addWebpackResolve } = require("customize-cra");
-
-// module.exports = override(
-//   addWebpackResolve({
-//     fallback: {
-//       buffer: require.resolve("buffer/"),
-//       util: require.resolve("util/"),
-//     },
-//   })
-// );
-
-
-
 // config-overrides.js
 const { override, addWebpackModuleRule } = require('customize-cra');
 
